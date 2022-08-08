bc7fd349194b2883b2e320a902efa386bd31adab
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2022-08-08 22:05:11 +0200

    .

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,21 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+for (let i = 0; i < galleryItems.length; i++) { 
+
+    const galleryHtml = `<a class="gallery__item" href="${galleryItems[i].original}">
+  <img class="gallery__image" src="${galleryItems[i].preview}" alt="${galleryItems[i].description}" />
+</a>`
+   
+    gallery.innerHTML += galleryHtml;
+
+}
+
+new SimpleLightbox(".gallery a", { captionDelay: 250 });
 
 
